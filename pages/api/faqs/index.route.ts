ca6ea38d9efe2b6e73485c3be0b662c5baa8e243
antoniomb370 <85515721+antoniomb370@users.noreply.github.com>
2024-03-25 20:38:29 -0500

    Creando la Api de faqs

---
+++ pages/api/faqs/index.route.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { faqsData } from '../../../components/faqs/faqsData' // Importa tus datos
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json(faqsData)
+}
